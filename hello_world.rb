25719c8d5e8256a9997df5ea79df180e51d4f1bc
Victorywastaken <rlatmdfl92@gmail.com>
2022-02-10 15:58:50 -0500

    added numbers

--- hello_world.rb
+++ hello_world.rb
@@ -1,3 +1,27 @@
 puts "hello world"
 # Runs Hello World
 puts "goodbye"
+# Runs Goodbye
+
+# "#" is a comment
+# comments are used to explain code
+# comments are ignored by the computer
+
+## Numbers
+# Example:
+puts "5 + 5 = #{5 + 5}"
+# This is a string interpolation
+# It will print 5 + 5
+
+puts 4 + 2.5   # 6.5
+puts 100 - 20  # 80
+puts 20 / 5    # 4
+puts 13 % 3    # 1
+
+puts 4 == 4       # true
+puts 5 + 3 != 7   # true
+puts 6 > 0        # true
+puts 6 < 0        # false
+puts 25 <= 25     # true
+puts 12 >= 16     # false
+
