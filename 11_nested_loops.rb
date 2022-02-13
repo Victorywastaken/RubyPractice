8a44cfb3b1d66e1aa1a788534572d16463ea90b2
Victorywastaken <rlatmdfl92@gmail.com>
2022-02-12 21:18:22 -0500

    learned basic ruby

--- 11_nested_loops.rb
+++ 11_nested_loops.rb
@@ -1,11 +1,12 @@
 # Nested Loops
 
+# prints 1, 2, 3
 (1..3).each do |num|
   puts num
 end
 
-# prints 1, 2, 3
 
+# nested num loop
 (1..3).each do |num1|
   puts num1
   (1..5).each do |num2|
@@ -25,3 +26,50 @@ end
 #   2
 #   3
 #...etc
+
+arr = ["a", "b", "c", "d"]
+
+# prints each element
+arr.each do |element|
+  puts element
+end
+
+# below we iterate through ALL possible with duplicates
+arr.each do |element1|
+  arr.each do |element2|
+    puts element1 + element2
+  end
+end
+
+# prints:
+# aa, ab, ac, ad, ba, bb, bc, bd, ca, cb, cc, cd, da, db, dc, dd
+
+
+# below we iterate through only unique pairs
+arr.each_with_index do |element2, i|
+  arr.each_with_index do |element1, j|
+    if j > i
+      puts element1 + element2
+    end
+  end
+end
+
+# if j is greater than i, then j is never referring to what i referred to previously
+
+
+# 2-D array
+
+arr = [
+  ["a", "b", "c"],
+  ["d", "e"],
+  ["f", "g", "h"]
+]
+
+print arr
+# [['a', 'b', 'c'], ['d', 'e'], ['f', 'g', 'h']]
+
+arr.each do |subArr|
+  subArr.each do |ele|
+
+  end
+end
--- 12_hashes.rb
+++ 12_hashes.rb
@@ -7,3 +7,147 @@ my_hash = {
   "age" => "5",
   "isAwesome" => true
 }
+
+# Hash Methods
+# .length
+# .has_key?(k)
+# .has_value?(v)
+# .keys
+# .values
+
+dog = {
+  "name" => "Fido",
+  "color" => "brown",
+  "age" => "3",
+  "isAwesome" => true
+  "enemies" => ["cats", "dogs"]
+}
+
+puts dog.length # 5
+
+dog["name"] = "Spot"
+dog["location"] = "NYC"
+
+puts dog
+# {
+#   "name"=>"Spot",
+#   "color"=>"brown",
+#   "age"=>"3",
+#   "isAwesome"=>true,
+#   "location"=>"NYC",
+#   "enemies"=>["cats", "dogs"]
+# }
+
+dog["enemies"] << "squirrels"
+puts dog
+# "enemes" => ["cats", "dogs", "squirrels"]
+
+dog = {
+  "name" => "Fido",
+  "color" => "brown",
+  "age" => "3",
+  "isAwesome" => true
+  "enemies" => ["cats", "dogs"]
+}
+
+dog.has_key("color") # true
+dog.has_key("location") # false
+
+dog.has_value("Fido") # true
+dog.has_value("5") # false
+
+print dog.keys
+# ["name", "color", "age", "isAwesome", "enemies"]
+
+print dog.values
+# ["Fido", "brown", "3", true, ["cats", "dogs"]]
+
+#Hash Enumerables
+# .each
+# .each_key
+# .each_value
+
+pizza = {
+  "style" => "New York",
+  "slices" => "8",
+  "diameter" => "12 inches",
+  "toppings" => ["cheese", "sauce", "pepperoni"]
+  "is_tasty" => true
+}
+
+pizza.each do |key, value|
+  puts key
+  puts value
+  puts "-----"
+end
+
+# "style"
+# "New York"
+# "-----"
+# "slices"
+# "8"
+# "-----"
+# "diameter"
+# "12 inches"
+# "-----"
+# "toppings"
+# ["cheese", "sauce", "pepperoni"]
+# "-----"
+# "is_tasty"
+# true
+
+
+pizza.each_key do |key|
+  puts key
+end
+# "style"
+# "slices"
+# "diameter"
+# "toppings"
+# "is_tasty"
+
+pizza.each_value do |value|
+  puts value
+end
+
+# "New York"
+# "8"
+# "12 inches"
+# ["cheese", "sauce", "pepperoni"]
+# true
+
+# Hashes
+# default value nil
+# Hash.new(default)
+# Counter Hash Strategy
+
+my_hash = {
+  "a" => 28
+}
+
+puts my_hash["b"] # nil
+my_hash["b"] == nil # true
+
+my_hash = Hash.new(0)
+
+puts my_hash # {}
+puts my_hash[a] # 0
+
+
+counter = Hash.new(0)
+str = "bootcamp prep"
+str.each_char { |char| counter[char] += 1 }
+
+# counter hash
+# sort_by
+
+counter.sort_by { |k, v| v }
+
+# nil
+#   represents "nothing" or "empty"
+#   common appears as a default value
+
+my_array = ["a", "b", "c"]
+print my_array[2] # "c"
+p my_array[3] # nil
+
